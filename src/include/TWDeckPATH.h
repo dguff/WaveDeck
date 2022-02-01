494fa47c0903223066b83375da25decd51a665f1
Dan Guff <daniele.guffanti@unimib.it>
2022-02-01 12:29:26 +0100

    WaveDeck Path definitions

---
+++ src/include/TWDeckPATH.h
@@ -0,0 +1,16 @@
+/**
+ * @author      : Daniele Guffanti (daniele.guffanti@mib.infn.it)
+ * @file        : TWDeckPATH
+ * @created     : martedì feb 01, 2022 09:57:36 CET
+ */
+
+#ifndef TWDECKPATH_H
+
+#define TWDECKPATH_H
+
+#define WDECK_INCLUDE "/home/guff/Dune/deconvolution/WaveDeck/src/include"
+#define WDECK_LIB_DIR "/home/guff/Dune/deconvolution/WaveDeck/lib"
+
+
+#endif /* end of include guard TWDECKPATH_H */
+
